9687d3f8ba934a98a0962d32708e023a33518fd1
MathieuGood <bon.mathieu@gmail.com>
2022-11-10 11:03:56 +0100

    Base for new version

--- sonos_home_ctrl.py
+++ sonos_home_ctrl.py
@@ -1,11 +1,20 @@
 import soco
 
 
+def get_spk_list():
+    spk_list = soco.discover()
+    if spk_list != None:
+        return list(spk_list)
+    else:
+        return "No Sonos speakers detected on this network"
+
+def get_spk_detail(spk):
+    ...
+    # return name and ip of speaker (and group?)
 
 
 def main():
-    ...
-
+    print(get_spk_list())
 
 
 
